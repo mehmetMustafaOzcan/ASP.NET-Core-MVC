1746d308a5a5401e6f689dd4249632bceaa37b8e
Mehmet Mustafa Özcan <122937138+mehmetMustafaOzcan@users.noreply.github.com>
2023-05-07 17:09:55 +0300

    added main branch

--- ManageTheNorthwind/ManageTheNorthwind/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ManageTheNorthwind.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
